972db71a3debf3a91139d7e1faf17cb3ce07080d
Leticia-Mendiola <lmendiola509@gmail.com>
2021-06-12 12:16:19 -0500

    realized multiple pages were unnecessary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Blog from "./components/pages/Blog";
-import Contact from "./components/pages/Contact";
+import OmdbContainer from "./components/OmdbContainer";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
-      </div>
-    </Router>
-  );
+  return <OmdbContainer />;
 }
 
 export default App;
