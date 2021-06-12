7bf0bf8446b6c71cc7e00ecc91cb9fe36b035999
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-12 17:25:18 -0500

    added function to just pull base/all data from API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?";
+const BASEURL = "https://randomuser.me/api";
 // const APIKEY = "&apikey=trilogy";
 
 export default {
+  getAll: function(query) {
+    return axios.get(BASEURL)
+  },
   search: function(query) {
-    return axios.get(BASEURL + query);
+    return axios.get(BASEURL + "/?" + query);
   }
 };
