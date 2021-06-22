ffb13ede762e211934e63437b81eff4827af8708
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 23:49:40 -0500

    corrected typo

--- src/App.js
+++ src/App.js
@@ -2,31 +2,14 @@ import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import EmployeeCard from "./components/EmployeeCard";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 import results from "./results.json";
 
 function App() {
   return (
     <Wrapper>
-      <Search />
+      {/* <Search /> */}
       <Title>Employees</Title>
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
-        </Container>
-      </div>
       { results.map(results => (
         <EmployeeCard
         first={results.name.first}
