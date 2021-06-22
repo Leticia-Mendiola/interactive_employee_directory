e9fda5d447dcb277a347824a8cbb93458c018b33
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 22:43:11 -0500

    added additional components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
-// import './App.css';
-// import React from "react";
-// import Home from "./pages/home";
-
-// function App() {
-//   return <Home />;
-// }
-
-// export default App;
-
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import EmployeeCard from "./components/EmployeeCard";
 import results from "./results.json";
+import API from "../utils/API";
+import Container from "../components/Container";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+import Alert from "../components/Alert";
+
+class Search extends Component {
+  state = {
+    search: "",
+    breeds: [],
+    results: [],
+    error: ""
+  };
+
+  // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  componentDidMount() {
+    API.getBaseBreedsList()
+      .then(res => this.setState({ breeds: res.data.message }))
+      .catch(err => console.log(err));
+  }
+
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getDogsOfBreed(this.state.search)
+      .then(res => {
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        this.setState({ results: res.data.message, error: "" });
+      })
+      .catch(err => this.setState({ error: err.message }));
+  };
+  render() {
+    return (
+      <div>
+        <Container style={{ minHeight: "80%" }}>
+          <h1 className="text-center">Search By Breed!</h1>
+          <Alert
+            type="danger"
+            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+          >
+            {this.state.error}
+          </Alert>
+          <SearchForm
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            breeds={this.state.breeds}
+          />
+          <SearchResults results={this.state.results} />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Search;
 
 function App() {
   return (
